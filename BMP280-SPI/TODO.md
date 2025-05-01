3248449cb11c42c7525b45658edb0126d2bdc203
PaszaVonPomiot <schedar@op.pl>
2025-05-02 01:05:39 +0200

    upd

--- BMP280-SPI/TODO.md
+++ BMP280-SPI/TODO.md
@@ -1,14 +1,1 @@
-- pinout config should only ask for pin number not the mode or value
-- Add uptime and rtc timestamp variants
-- Change to create file only when during save it doesn't exist
-- Add gzip
-- boot.py to set internal rtc from external rtc
-- logger should write to one file <data/sensorname.csv>
-- implement timestamp
-- add gc.collect after saving buffer to file
-
-- buffer size to config
-- add gzip compression with "gzip.open(file_path, mode="at")"
-
-
-
+- add gzip compression with "gzip.open(file_path, mode="a")"
--- BMP280-SPI/main.py
+++ BMP280-SPI/main.py
@@ -1,15 +1,20 @@
-"""
-Set correct SensorLogger.buffer to avoid MemoryError (buffer~183 seems to be max from host, but from Pico buffer~???)
-"""
+import time
 
 from core.bmp280 import BMP280Sensor
+from core.logger import LoggerCSV
 
 
 def main():
     sensor = BMP280Sensor()
-    print(sensor.get_csv_record())
-    print(sensor.get_readout())
-    print(sensor.get_csv_record())
+    logger = LoggerCSV(
+        file_name="bmp280.csv",
+        csv_headers=["timestamp", "temperature", "pressure"],
+        buffer_size=180,
+    )
+
+    while True:
+        logger.process_record(record=sensor.get_csv_record())
+        time.sleep(10.0)
 
 
 if __name__ == "__main__":
