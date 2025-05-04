200cb040ddc7889a6470f4c3ec84dc59ec53351f
PaszaVonPomiot <schedar@op.pl>
2025-05-04 13:36:40 +0200

    upd

--- BMP280-SPI/boot.py
+++
@@ -1,8 +0,0 @@
-import time
-
-import machine
-from config.board import Pico
-
-time.sleep(2)
-machine.freq(Pico.MCU_FREQUENCY)
-machine.RTC().datetime(Pico.RTC_DATETIME)  # sync internal RTC
--- Blink/main.py
+++
@@ -1,9 +0,0 @@
-import time
-
-from machine import Pin
-
-led = Pin("LED", mode=Pin.OUT)
-
-while True:
-    led.toggle()
-    time.sleep(0.5)
